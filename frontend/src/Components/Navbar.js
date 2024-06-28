62134196b87aaa5e4fc23d03e929788a0d7d2244
nicoferrey <nikferreyra@gmail.com>
2024-06-28 03:33:14 -0300

    nabvar with links

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,9 +1,60 @@
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { IoMdExit } from "react-icons/io";
 import logo from '../images/paseapp-logo.png';
+import { useNavigate } from 'react-router-dom';
+
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    const userData = JSON.parse(window.sessionStorage.getItem('user'));
+    const userType = window.sessionStorage.getItem('userType');
+
+    const handleLogout = () => {
+        // Limpiar sessionStorage
+        window.sessionStorage.removeItem('user');
+        window.sessionStorage.removeItem('userType');
+        // Redireccionar al login
+        navigate('/login');
+    };
+
+    const redirectToProfile = () => {
+        if (userData) {
+            if (userType === 'paseador') {
+                navigate('/profile');
+            } else {
+                navigate('/profile');
+            }
+        } else {
+            navigate('/login');
+        }
+    };
+
+    const redirectToServices = () => {
+        if (userData) {
+            if (userType === 'paseador') {
+                navigate('/profile');
+            } else {
+                navigate('/services');
+            }
+        } else {
+            navigate('/login');
+        }
+    };
+
+    const redirectToRequests = () => {
+        if (userData) {
+            if (userType === 'paseador') {
+                navigate('/requests');
+            } else {
+                navigate('/profile');
+            }
+        } else {
+            navigate('/login');
+        }
+    };
+
     return ( 
         <nav className="navbar">
         <div className="navbar-container container">
@@ -14,9 +65,10 @@ const Navbar = () => {
                 <span className="line line3"></span>
             </div>
             <ul className="menu-items">
-                <li><a href="#">Servicios</a></li>
-                <li><a href="#">Pedidos</a></li>
-                <li><a href="#"><FontAwesomeIcon icon={faUser} /></a></li>
+                <li><a onClick={redirectToServices}>Servicios</a></li>
+                <li><a onClick={redirectToRequests}>Pedidos</a></li>
+                <li><a onClick={redirectToProfile}><FontAwesomeIcon icon={faUser} /></a></li>
+                <li><a onClick={handleLogout}><IoMdExit size={22} /></a></li>
             </ul>
             <div className="logo"><img src={ logo } alt="PASEAPP" /></div>
         </div>
